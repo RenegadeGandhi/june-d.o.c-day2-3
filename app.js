9046397ed82ed970d745d72afd7b47433d4a5294
RenegadeGandhi <osinayaoludare@gmail.com>
2020-06-13 13:38:26 -0600

    user

--- app.js
+++ app.js
@@ -11,10 +11,13 @@ app.use(bodyParser.json())
 
 
 
-app.get('/users', (req, res, next) => {
+
+
+app.get('/', (req, res, next) => {
     fetch('http://jsonplaceholder.typicode.com/users')
     .then(res => res.json())
     .then(json => {
+        let arr = [];
         for (let i = 0; i < json.length; i++) {
             let users = {
                 id: json[i].id,
@@ -22,15 +25,104 @@ app.get('/users', (req, res, next) => {
                 username: json[i].username,
                 email: json[i].email,
             }
-            onsole.log(users);
+            arr.push(users);
+
+            stringUsers = JSON.stringify(arr, null, 2);
+            fs.writeFile('./users.json', stringUsers, function (err) {
+                if (err) throw err;
+            });
         }
-    });
-    /*fs.appendFile('users.json', word, function (err) {
-        if (err) throw err;
-        res.status(200).send("Word Added!");
-    });*/
+        return res.send("Welcome")
+    }).catch(err => err)
 });
 
+
+app.get('/users', (req, res, next) => {
+    fs.readFile('./users.json', function(err, data) {
+        res.write(data);
+        return res.end();
+    });
+});
+
+app.get('/name', (req, res, next) => {
+    fs.readFile('./users.json', 'utf8', function(err, data) {
+        let arrNames = [];
+        let pData = JSON.parse(data);
+        for (let i = 0; i < pData.length; i++) {
+            let names = {
+                name: pData[i].name
+            }
+            arrNames.push(names);
+        }
+
+        res.send(arrNames);
+    });
+});
+
+app.get('/email', (req, res, next) => {
+    fs.readFile('./users.json', 'utf8', function(err, data) {
+        let arrEmail = [];
+        let eData = JSON.parse(data);
+        for (let i = 0; i < eData.length; i++) {
+            let emails = {
+                email: eData[i].email
+            }
+            arrEmail.push(emails);
+        }
+
+        res.send(arrEmail);
+    });
+});
+
+app.get('/username', (req, res, next) => {
+    fs.readFile('./users.json', 'utf8', function(err, data) {
+        let arrUsernames = [];
+        let uData = JSON.parse(data);
+        for (let i = 0; i < uData.length; i++) {
+            let usernames = {
+                username: uData[i].username
+            }
+            arrUsernames.push(usernames);
+        }
+
+        res.send(arrUsernames);
+    });
+});
+
+app.get('/delete', (req, res, next) => {
+    fs.readFile('./users.json', 'utf8', function(err, data) {
+        let newData = JSON.parse(data);
+        let x = newData.pop();
+        res.send(newData);
+    });
+});
+
+app.get('/deleteall', (req, res, next) => {
+    fs.readFile('./users.json', 'utf8', function(err, data) {
+        let newerData = JSON.parse(data);
+        newerData.splice(0, 10);
+        res.send(newerData);
+    });
+});
+
+app.get('/newuser', (req, res, next) => {
+    fs.readFile('./users.json', 'utf8', function(err, data) {
+        let newuser = JSON.parse(data);
+
+        /*const newId = (newuser) => {
+            if (newuser.length > 0) {
+                return array[array.length - 1].id + 1
+            } else {
+                return 1
+            }
+        }*/
+
+        /*const { name, email, username } = req.body
+        if
+        newuser.push(req.body);
+        res.send(newerData);*/
+    });
+});
 
 app.use((req, res, next) => {
     res.status(302).send(`Head over to /users`);
